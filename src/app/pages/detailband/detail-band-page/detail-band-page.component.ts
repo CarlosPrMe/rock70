0dbaf1dafb4cb05b5526bcd9fc5b138bf2d3fe97
Carlos Prieto <carlospm86@gmail.com>
2020-06-09 17:53:48 +0200

    style on details page

--- src/app/pages/detailband/detail-band-page/detail-band-page.component.ts
+++ src/app/pages/detailband/detail-band-page/detail-band-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { BandModel } from 'src/app/models/band.model';
 import { MockService } from 'src/app/services/mockService.service';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-detail-band-page',
@@ -10,19 +11,30 @@ import { MockService } from 'src/app/services/mockService.service';
 })
 export class DetailBandPageComponent implements OnInit {
 
-  public band : BandModel;
+  public band: BandModel;
   public showDetails: boolean;
-  constructor(private _activate: ActivatedRoute, private _mockService: MockService) { }
+  private _confirmDelete: boolean;
+  constructor(private _activate: ActivatedRoute, private _mockService: MockService, private _saneticer: DomSanitizer) { }
 
   ngOnInit(): void {
     this.band = this._activate.snapshot.data.band;
     this.band.members.forEach(m => m.image = this._mockService.setAvatar()); //Mock to show some images. Better with real photos
     this.band.discography.forEach(a => a.cover = this._mockService.setCover()); //Mock to show some images. Better with real photos
     this.showDetails = false;
+    this._confirmDelete = false;
   }
 
-  public showMore(){
+  public showMore() {
     this.showDetails = !this.showDetails
   }
 
+  public videoLink(video) {
+    return this._saneticer.bypassSecurityTrustResourceUrl(video);
+  }
+
+  public deleteBand() {
+    this._confirmDelete = confirm('¿Seguro que quieres eliminar el grupo?');
+    console.log(this._confirmDelete);
+    
+  }
 }
